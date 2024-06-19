2a17924bec8621807aebc26c2b997d057fbec685
yysrll <yusril.uzumaki@gmail.com>
2024-06-19 20:10:20 +0700

    fix: add transition hover to ProjectCard

--- src/app/_components/ProjectCard.tsx
+++ src/app/_components/ProjectCard.tsx
@@ -18,7 +18,7 @@ export default function ProjectCard({
   description,
 }: ProjectCardProps) {
   return (
-    <div className="cursor-pointer rounded-lg bg-gray-500/10 p-4 backdrop-blur-sm dark:bg-white/20">
+    <div className="cursor-pointer rounded-lg bg-gray-500/10 p-4 backdrop-blur-sm transition hover:scale-110 hover:bg-gray-500/30 dark:bg-white/20 dark:hover:bg-white/30">
       <div className="relative aspect-video w-full">
         <Image
           src={imageSrc}
@@ -30,7 +30,7 @@ export default function ProjectCard({
         />
       </div>
       <div className="p-6">
-        <h2 className="text-md font-semibold text-black md:text-xl dark:text-white">
+        <h2 className="text-md font-semibold text-black dark:text-white md:text-xl">
           {title}
         </h2>
         <p className="mt-2 line-clamp-3 overflow-hidden text-ellipsis text-sm leading-6 text-gray-500 dark:text-gray-400">
