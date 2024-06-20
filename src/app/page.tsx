f48a8fc22839f1d6659dfb90e829daa06da73a38
yysrll <yusril.uzumaki@gmail.com>
2024-06-20 15:54:12 +0700

    fix: projects padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main>
       <div className="bg-hero-pattern bg-top bg-no-repeat">
-        <div className="flex flex-col items-center justify-center bg-hero-pattern-full bg-right bg-no-repeat">
+        <div className="flex flex-col items-center justify-center bg-hero-pattern-full bg-right bg-no-repeat px-12 py-12 md:px-32 lg:px-48">
           <Hero />
           <ProjectList projects={projects.slice(0, 3)} />
           <ButtonTransparent href="/project">Load More..</ButtonTransparent>
--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div className="bg-hero-pattern-top bg-no-repeat">
       <div className="bg-hero-pattern-full bg-center bg-no-repeat">
-        <div className="flex flex-col items-center px-12 pt-24 md:px-32 lg:px-48">
+        <div className="flex flex-col items-center px-12 pt-32 md:px-32 lg:px-48">
           <ProjectList projects={projects} />
         </div>
       </div>
