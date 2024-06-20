c8d948544386c8d09b385cb7d8328016111fa8b6
yysrll <yusril.uzumaki@gmail.com>
2024-06-20 15:03:14 +0700

    feat: add more projects

--- data/ProjectData.tsx
+++ data/ProjectData.tsx
@@ -4,21 +4,35 @@ const projects = [
     description:
       "This mobile application for ultimate all-in-one solution for optimizing your workday at PT Pos Logistics Indonesia. This internal app is designed to streamline your daily tasks, enhance collaboration, and boost productivity like never before.",
     image: "/projects/oneposlog.png",
-    link: "",
+    link: "/",
   },
   {
     title: "Pakarangan App",
     description:
       "The Pakarangan application is designed to check the availability of glamping sites at the Pakarangan tourist destination.",
     image: "/projects/pakarangan.png",
-    link: "https://github.com",
+    link: "/",
+  },
+  {
+    title: "RPMC App",
+    description:
+      "Application created for repair services for refrigerators or called the Repair and Preventive Maintenance Center (RPMC) Project",
+    image: "/projects/rpmc.png",
+    link: "/",
+  },
+  {
+    title: "TMS App",
+    description:
+      "This Application use for PT Pos Logistik drivers to ship their package from origin to destination",
+    image: "/projects/tms.png",
+    link: "/",
   },
   {
     title: "I-Talk App",
     description:
       "The I-talk application is used as a learning tool for children to recognize objects or images with modifiable voice features.",
     image: "/projects/I-Talk.png",
-    link: "https://github.com",
+    link: "/",
   },
 ];
 
--- src/app/_components/ProjectList.tsx
+++ src/app/_components/ProjectList.tsx
@@ -1,26 +1,32 @@
 import projects from "../../../data/ProjectData";
 import ProjectCard from "./ProjectCard";
 
-export default function ProjectList() {
+interface ProjectProps {
+  image: string;
+  title: string;
+  description: string;
+}
+
+interface ProjectListProps {
+  projects: ProjectProps[];
+}
+
+export default function ProjectList({ projects }: ProjectListProps) {
   return (
     <div className="px-12 py-12 md:px-32 lg:px-48">
       <div className="flex flex-col items-center">
-        <h2 className="text-xl font-bold text-black md:text-2xl dark:text-white">
+        <h2 className="text-xl font-bold text-black dark:text-white md:text-2xl">
           👨‍💻 Recent Projects
         </h2>
-        <p className="mb-12 text-gray-600 dark:text-gray-400">Here are some of my projects</p>
+        <p className="mb-12 text-gray-600 dark:text-gray-400">
+          Here are some of my projects
+        </p>
       </div>
       <div className="grid-col-1 grid gap-12 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
         {projects.map((project, index) => (
-          <ProjectCard
-            key={index}
-            imageSrc={project.image}
-            title={project.title}
-            description={project.description}
-          />
+          <ProjectCard key={index} {...project} />
         ))}
       </div>
     </div>
   );
 }
-
