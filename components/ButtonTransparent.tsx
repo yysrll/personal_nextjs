9a7892583df23994b96ae782b3bea28020fb6391
yysrll <yusril.uzumaki@gmail.com>
2024-06-20 15:11:23 +0700

    fix: add load more button

--- components/ButtonTransparent.tsx
+++ components/ButtonTransparent.tsx
@@ -14,7 +14,7 @@ const ButtonTransparent: React.FC<ButtonTransparentProps> = ({
   return (
     <a
       target={target}
-      className="flex h-12 items-center justify-center rounded-xl bg-gray-700/50 px-4 text-white backdrop-blur-lg transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-white hover:text-black dark:bg-white/20 dark:hover:text-white"
+      className="flex h-12 w-fit items-center justify-center rounded-xl bg-gray-500/30 px-4 text-white backdrop-blur-lg transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:border hover:border-black hover:bg-white hover:text-black dark:bg-white/20 dark:hover:text-white"
       href={href}
     >
       {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import ButtonTransparent from "../../components/ButtonTransparent";
 import projects from "../../data/ProjectData";
 import Hero from "./_components/Hero";
 import ProjectList from "./_components/ProjectList";
@@ -6,9 +7,10 @@ export default function Home() {
   return (
     <main>
       <div className="bg-hero-pattern bg-top bg-no-repeat">
-        <div className="bg-hero-pattern-full bg-right bg-no-repeat">
+        <div className="flex flex-col items-center justify-center bg-hero-pattern-full bg-right bg-no-repeat">
           <Hero />
           <ProjectList projects={projects.slice(0, 3)} />
+          <ButtonTransparent href="/project">Load More..</ButtonTransparent>
         </div>
       </div>
     </main>
